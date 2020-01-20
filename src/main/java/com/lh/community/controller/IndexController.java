a1b517bfc792beb39eeb9db1d04b9ad417380ef6
jack_glb <likefile@jk.com>
2020-01-20 15:53:28 +0800

    add index and bootstrap

---
+++ src/main/java/com/lh/community/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.lh.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author lanhu
+ * @create 2020-01-20 14:51
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
